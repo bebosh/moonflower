0c2f78c6515ed2aff5b91c89c906f799b99dc972
Paolo <pieffelleo@gmail.com>
2019-05-09 11:23:59 +0200

    added sidebar toggle

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-const Sidebar = () => {
+
+const Sidebar = props => {
+  
+  var visible = " sideBar hidden";
+
+  if (props.show) {
+    visible = "sideBar visible";
+  }
+
   return (
-    <ul className="right">
-      <li>
-        <NavLink exact to="/" className="nav_title">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/About" className="nav_title">
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/Contacts" className="nav_title">
-          Contacts
-        </NavLink>
-      </li>
-    </ul>
+    <div >
+      <ul className={visible}>
+        <li>
+          <NavLink exact to="/" className="nav_title">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/About" className="nav_title">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Contacts" className="nav_title">
+            Contacts
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 export default Sidebar;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import logoNavbar from "../images/flower.navbar.svg";
 import Sidebar from "./Sidebar";
+
+
 class Navbar extends Component {
   constructor(props) {
     super(props);
@@ -23,12 +25,12 @@ class Navbar extends Component {
             <a href="/" className="brand-logo">
               <img src={logoNavbar} className="App-logo-navbar" alt="logo" />
             </a>
-            <span class="right grey-text text-darken-1">
-              <i class="material-icons sidenav-trigger" data-target="side-menu" onClick={this.onClickToggle}>
+            <span className="right grey-text text-darken-1">
+              <i className="material-icons sidenav-trigger" data-target="side-menu" onClick={this.onClickToggle}>
                 menu
               </i>
             </span>
-            { this.state.show ? <Sidebar /> : null }
+            <Sidebar show={this.state.show}/>
             
           </div>
         </nav>
